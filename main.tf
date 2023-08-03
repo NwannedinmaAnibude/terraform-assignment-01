a7abfff9684c7f765ad9b16b9122b3108a8a0302
NwannedinmaAnibude <134140003+NwannedinmaAnibude@users.noreply.github.com>
2023-08-03 17:20:45 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@
 
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "nwanne" # Replace this with your desired bucket name
-  acl    = "public" # You can change the ACL (Access Control List) as per your requirements
+  acl    = "public-read" # You can change the ACL (Access Control List) as per your requirements
 }
